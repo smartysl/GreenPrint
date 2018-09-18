94ee9e84c7282a904288800d6a95136839e0ab72
smartysl <1632892359@qq.com.com>
2018-09-18 21:52:37 +0800

    完善注册功能

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
+import base64
 BASE_DIRS=os.path.dirname(__file__)
 options={
     'port':8080,
 }
 settings={
     'static_path':os.path.join('static',BASE_DIRS),
+    'cookie_secret':base64.b64encode('于轼霖最帅'),
     'template_path':os.path.join(BASE_DIRS,'templates'),
     'debug':True
 }
--- views/index.py
+++ views/index.py
@@ -3,15 +3,16 @@ sys.path.append('..')
 import tornado.web
 import re
 import application
+import random
 class MainHandler(tornado.web.RequestHandler):
     def get(self, *args, **kwargs):
         self.write('hello world')
 class UserRegisterHanlder(tornado.web.RequestHandler):
-    def get(self, *args, **kwargs):
+    def post(self, *args, **kwargs):
         context={}
-        email=self.get_query_argument('email',default=None)
-        password=self.get_query_argument('password')
-        password_again=self.get_query_argument('password_again')
+        email=self.get_argument('email',default=None)
+        password=self.get_argument('password')
+        password_again=self.get_argument('password_again')
         if len(password)<8:
             context['password_length_error']='密码至少8个字符'
         if password_again!=password:
@@ -21,6 +22,10 @@ class UserRegisterHanlder(tornado.web.RequestHandler):
             db=application.Application().db
             sql='insert into user (password) values (%s)'
             db.insert(sql,password)
+            account=random.randint(10000000,99999999)
+            sql=' insert into user_info (account,email) values (%s,%s)'
+            db.insert(sql,account,email)
+            self.write('ok')
 
 
 
